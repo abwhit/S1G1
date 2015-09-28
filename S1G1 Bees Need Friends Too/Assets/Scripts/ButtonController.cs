33e0a6e996ce7c865a4ac0fb789eeca1eb11704d
abwhit <1002406@student.sae.edu.au>
2015-09-28 14:04:40 +1000

    Game over screen and UI added

---
+++ S1G1 Bees Need Friends Too/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+
+
+	public void OnClickRestart()
+	{
+		Application.LoadLevel ("GameScene");
+	}
+
+	public void OnClickMenu()
+	{
+		Application.LoadLevel ("Menu");
+
+	}
+
+}
