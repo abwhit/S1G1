83842e482c2433eee6ece7beff8320f0ec5f1bca
abwhit <1002406@student.sae.edu.au>
2015-09-28 13:55:09 +1000

    Second Initial Commit

---
+++ S1G1 Bees Need Friends Too/Assets/Scripts/BeeControls.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeeControls : MonoBehaviour {
+
+    // Store all the movement variables in floats
+    public float speed = 0f;
+    private float horiMovement = 0f;
+    private float vertMovement = 0f;
+    // Link the rigidBody of this gameObject
+    private Rigidbody2D RB;
+    // Bool to change the control scheme
+    public bool bee1 = false;
+
+    //public Collectable collectables;
+
+    public GameController gameController;
+
+    public GameObject otherBee;
+
+    public float distance;
+	public float direction;
+
+
+
+
+    void Awake()
+    {
+        // Get the rigidBody component of this gameObject
+        RB = GetComponent<Rigidbody2D>();
+
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+
+    }
+    // Use this for initialization
+    void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        distance = Vector2.Distance(transform.position, otherBee.transform.position);
+
+
+        DistanceCheck();
+
+		if (gameController.beesAreClose) {
+
+			speed = 10f;
+		} else {
+			speed = 5f;
+
+		}
+
+	}
+
+    void FixedUpdate()
+    {
+        // Check which bee is is and alter the controls accordingly
+        if (bee1)
+        {
+            // Store the axis in the floats created above
+            horiMovement = Input.GetAxis("Horizontal");
+            vertMovement = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            horiMovement = Input.GetAxis("Horizontal2");
+            vertMovement = Input.GetAxis("Vertical2");
+        }
+        
+
+
+			// Change the velocity of the object using the speed and movement floats
+			RB.velocity = new Vector2(horiMovement * speed, vertMovement * speed);
+	
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("111");
+        if (other.gameObject.tag == "Collectable")
+        {
+            if (other.gameObject.GetComponent<Collectable>().IsCollectable)
+            {
+                gameController.score += other.gameObject.GetComponent<Collectable>().scoreIncrease;
+                Destroy(other.gameObject);
+                Debug.Log("111");
+            }
+        }
+		if(other.gameObject.GetComponent<BeeControls>() != null)
+		{
+			Debug.Log("222");
+
+			if(gameController.score > 0)
+			{
+			gameController.score -= 200;
+				if(gameController.score < 0)
+				{
+					gameController.score = 0;
+				}
+			}
+
+
+			
+		}
+
+    }
+
+   void whenHitBee()
+	{
+
+
+
+
+
+	}
+
+
+    void DistanceCheck()
+    {
+        if (distance <= 2f)
+        {
+            gameController.beesAreClose = true;
+        }
+        else
+        {
+            gameController.beesAreClose = false;
+        }
+
+    }
+
+
+}
---
+++ S1G1 Bees Need Friends Too/Assets/Scripts/GameController.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+   
+
+    // Information about storing the time
+    public float timeS = 59;
+    public int timeM = 1;
+
+    // Bool to check if the game is playing
+    bool isPlaying;
+
+    // Storing the timerText
+    public Text timerText;
+
+    // Storing the score and the score text
+    public int score = 0;
+    public Text scoreText;
+
+	public int highScore;
+	public int highScoreText;
+
+    public bool beesAreClose;
+
+	public bool gameOver;
+
+	public GameObject gameOverScreen;
+
+	public Text GOScoreText;
+	public Text GOHSText;
+
+	public bool reload;
+
+
+    
+
+    void Awake()
+    {
+        
+
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        isPlaying = true;
+        beesAreClose = false;
+
+		highScore = PlayerPrefs.GetInt ("HighScore", 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timerText.text = timeM.ToString("0") + ":" + timeS.ToString("00");
+
+        scoreText.text = "Score: " + score.ToString();
+
+
+        if (isPlaying)
+        {
+            timer();  
+        }
+
+		if (gameOver) {
+
+			isPlaying = false;
+			endGameOptions();
+		}
+
+		if (reload) {
+
+			reset ();
+		}
+	
+	}
+
+	void endGameOptions()
+	{
+
+
+		if (score > highScore) {
+
+			PlayerPrefs.SetInt("HighScore", score);
+
+		}
+		GOScoreText.text = "Round Score: " + score.ToString ();
+		GOHSText.text = "Current Highscore: " + highScore.ToString ();
+
+		gameOverScreen.SetActive (true);
+
+
+	}
+
+	void reset()
+	{
+		Application.LoadLevel ("GameScene");
+
+	}
+
+
+
+    void timer()
+    {
+        timeS -= Time.deltaTime * 1;
+
+        if (timeS <= 0.01f)
+        {
+            timeS = 59f;
+            timeM -= 1;
+
+        }
+        if (timeM < 0)
+        {
+			gameOver = true;
+        }
+
+    }
+}
