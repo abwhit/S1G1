34cb18eaea10306b31e642a6e4a52e068cb4080e
abwhit <1002406@student.sae.edu.au>
2015-09-28 14:38:41 +1000

    Pointer Added

---
+++ S1G1 Bees Need Friends Too/Assets/Scripts/PointerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointerScript : MonoBehaviour {
+
+	public Transform player;
+
+	public GameObject pointer;
+	// Use this for initialization
+	void Start () {
+		pointer.SetActive (false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.LookAt (player);
+
+		if (player.gameObject.GetComponent<BeeControls> ().distance > 10) {
+
+			pointer.SetActive(true);
+
+		}
+		else{
+			pointer.SetActive(false);
+		}
+
+	}
+}
